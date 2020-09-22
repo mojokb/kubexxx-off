1271fcd6e0fb15a1614a34ae29ab78b02aee5c61
Lee MyoungHwan <amaramusic@gmail.com>
2020-09-22 12:08:56 +0900

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,3 @@
+/opt/conda/bin/jupyter notebook --generate-config
+cp /root/.bashrc /home/jovyan/
+/opt/conda/bin/jupyter lab --notebook-dir=/home/jovyan --ip=0.0.0.0 --no-browser --allow-root --port=8888 --LabApp.token='' --LabApp.password='' --LabApp.allow_origin='*' --LabApp.base_url=${NB_PREFIX}
